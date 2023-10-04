5472288f685a518f116132d5db85fcf7b03fa7f4
TshiamoTodd <tshiamo.mokwena19@gmail.com>
2023-10-04 12:10:09 +0200

    fix maxDuration error on api

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,7 +6,7 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
 import { NotificationType } from "@/types";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300;
+export const maxDuration = 1;
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
